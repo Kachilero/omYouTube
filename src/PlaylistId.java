006d9faf07765cd49678f4d4ba22d32022657db0
Alejandro <alejandrocerro@Alejandros-MacBook-Pro.local>
2015-04-07 12:23:24 -0500

    Initial commit

---
+++ src/PlaylistId.java
@@ -0,0 +1,9 @@
+package com.alejandro.omYouTube;
+
+import com.alejandro.omYouTube.YouTubeId;
+
+public class PlaylistId extends YouTubeId{
+    public PlaylistId(String pId){
+        super(pId);
+    }
+}
