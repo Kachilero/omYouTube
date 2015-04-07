145cd27d5a4f0c1e6de37fea922f78afee241f1c
Alejandro <alejandrocerro@Alejandros-MacBook-Pro.local>
2015-04-07 15:03:03 -0500

    moved all the java classes

--- src/PlaylistId.java
+++
@@ -1,9 +0,0 @@
-package com.alejandro.omYouTube;
-
-import com.alejandro.omYouTube.YouTubeId;
-
-public class PlaylistId extends YouTubeId{
-    public PlaylistId(String pId){
-        super(pId);
-    }
-}
--- src/VideoId.java
+++
@@ -1,9 +0,0 @@
-package com.alejandro.omYouTube;
-
-import com.alejandro.omYouTube.YouTubeId;
-
-public class VideoId extends YouTubeId{
-    public VideoId(String pId){
-        super(pId);
-    }
-}
